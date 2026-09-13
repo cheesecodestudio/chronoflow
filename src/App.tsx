7c1978fa163112642d492dd8dabdb0a3db847111
Keslerth <cheesecodestudio@gmail.com>
2026-09-12 20:48:54 -0600

    feat(auth): add session management provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,22 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 
 import { ManagePage } from './pages/ManagePage'
 import { PresentationPage } from './pages/PresentationPage'
+import { AuthProvider } from './features/auth/AuthProvider'
 import { SettingsProvider } from './features/timers/SettingsContext'
 
 function App() {
   return (
-    <SettingsProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/manage" element={<ManagePage />} />
-          <Route path="/view" element={<PresentationPage />} />
-          <Route path="*" element={<Navigate to="/manage" replace />} />
-        </Routes>
-      </BrowserRouter>
-    </SettingsProvider>
+    <AuthProvider>
+      <SettingsProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/manage" element={<ManagePage />} />
+            <Route path="/view" element={<PresentationPage />} />
+            <Route path="*" element={<Navigate to="/manage" replace />} />
+          </Routes>
+        </BrowserRouter>
+      </SettingsProvider>
+    </AuthProvider>
   )
 }
 
